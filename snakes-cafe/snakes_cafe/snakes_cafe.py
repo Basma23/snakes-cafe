0eeeef64967af3b866d1c89167d1b3db1776a8ca
Basma Nizar Mohammed Eid <basmanizar@outlook.com>
2020-08-16 21:55:39 +0300

    Almost Done

--- snakes-cafe/snakes_cafe/snakes_cafe.py
+++ snakes-cafe/snakes_cafe/snakes_cafe.py
@@ -7,25 +7,32 @@ print('''**************************************
 
 Appetizers
 ----------
+**********
 Wings
 Cookies
 Spring Rolls
 ------------
+************
 Entrees
 -------
+*******
 Salmon
 Steak
 Meat Tornado
 A Literal Garden
 ----------------
+****************
 Desserts
 --------
+********
 Ice Cream
 Cake
 Pie
 ---
+***
 Drinks
 ------
+******
 Coffee
 Tea
 Unicorn Tears
