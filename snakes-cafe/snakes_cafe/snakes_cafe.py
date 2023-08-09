f23e2be16882f7412a3216ff104970917a2fb636
Basma Nizar Mohammed Eid <63533086+Basma23@users.noreply.github.com>
2023-08-09 16:00:05 +0300

    Update snakes_cafe.py

--- snakes-cafe/snakes_cafe/snakes_cafe.py
+++ snakes-cafe/snakes_cafe/snakes_cafe.py
@@ -32,60 +32,36 @@ def snakes_cafe():
     Tea
     Unicorn Tears
 
-    ************************************* 
-    What would you like to order? **
+    ***********************************
+    ** What would you like to order? **
     ***********************************
     ''')
-    menu_list = [
-    {
-        "plate": "Appetizers",
-        "items": [
-            "Wings",
-            "Cookies",
-            "Spring Rolls"
-        ],
-    },
-    {
-        "plate": "Entrees",
-        "items": [
-            "Salmon",
-            "Steak",
-            "Meat Tornado"
-            "A Literal Garden"
-        ],
-    },
-    {
-        "plate": "Desserts",
-        "items": [
-            "Ice Cream",
-            "Cake",
-            "Pie"
-        ],
-    },
-    {
-        "plate": "Drinks",
-        "items": [
-            "Coffee",
-            "Tea",
-            "Unicorn Tears"
-        ],
-    },
-    ]
 
-    items = ["Wings","Cookies","Spring Rolls","Salmon","Steak","Meat Tornado","A Literal Garden","Ice Cream","Cake","Pie","Coffee","Tea","Unicorn Tears"]
-    orders = []
-    entering = input()
-    while entering != 'quit':
-        if entering in items:
-            orders.append(entering)
-            number_of_order = orders.count(entering)
-            print("** %d order of %s have been added to your meal **" %(number_of_order, entering))
+    items = ['Wings', 'Cookies', 'Spring Rolls',
+             'Salmon', 'Steak', 'Meat Tornado', 'A Literal Garden',
+             'Ice Cream', 'Cake', 'Pie',
+             'Coffee', 'Tea', 'Unicorn Tears']
+
+    order = []
+
+    userOrder = input('> ').title()
+
+    while userOrder.lower() != 'quit':
+        if userOrder in items:
+            order.append(userOrder)
+            numberOfDishs = order.count(userOrder)
+            if numberOfDishs == 1:
+                print('** %d order of %s have been added to your meal **' %
+                      (numberOfDishs, userOrder))
+            if numberOfDishs > 1:
+                print('** %d orders of %s have been added to your meal **' %
+                      (numberOfDishs, userOrder))
         else:
-                print('Sorry, the meal that you ordered is not on our menu, anything else?')
-    
-        entering = input()
+            print('Sorry, your order is not in the menu, anything else?')
+        userOrder = input('> ').title()
 
-if __name__ == "__main__":
+    print('Your order is: ' + ', '.join(order) + '.')
+
+
+if __name__ == '__main__':
     print(snakes_cafe())
-
-
